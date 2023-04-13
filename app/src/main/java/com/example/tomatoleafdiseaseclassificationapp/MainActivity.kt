993bd92c8adfb9ce1fae56a72240bdde79c5b2b0
RomontiBogdan <romontibogdan@gmail.com>
2023-04-13 13:01:48 +0300

    project innit

---
+++ app/src/main/java/com/example/tomatoleafdiseaseclassificationapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tomatoleafdiseaseclassificationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tomatoleafdiseaseclassificationapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+    }
+}
