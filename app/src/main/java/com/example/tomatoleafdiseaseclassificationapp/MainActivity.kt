9ea2a8afcce7845bfe7cc0f0662c3bab8f551792
RomontiBogdan <romontibogdan@gmail.com>
2023-05-02 12:22:46 +0300

    Login and Main Page

--- app/src/main/java/com/example/tomatoleafdiseaseclassificationapp/MainActivity.kt
+++ app/src/main/java/com/example/tomatoleafdiseaseclassificationapp/MainActivity.kt
@@ -1,41 +1,82 @@
 package com.example.tomatoleafdiseaseclassificationapp
 
+
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
-import com.example.tomatoleafdiseaseclassificationapp.databinding.ActivityCameraBinding
+import androidx.core.view.GravityCompat
 import com.example.tomatoleafdiseaseclassificationapp.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
+import com.example.tomatoleafdiseaseclassificationapp.databinding.HeaderNavigationDrawerBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var activityMainBinding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), AuthStateListener, NavigationView.OnNavigationItemSelectedListener{
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(activityMainBinding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        activityMainBinding.cameraIcon.setOnClickListener {
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        binding.cardHistory.cardImageView.setImageResource(R.drawable.history_icon)
+        binding.cardExplore.cardImageView.setImageResource(R.drawable.explore_icon)
+
+        binding.cameraIcon.setOnClickListener {
             val intent = Intent(this, CameraActivity::class.java)
             startActivity(intent)
         }
 
-        activityMainBinding.toolbar.setNavigationOnClickListener {
-            activityMainBinding.drawerLayoutMenu.open()
+        binding.toolbar.setNavigationOnClickListener {
+            binding.drawerLayoutMenu.open()
         }
 
-        activityMainBinding.navigationView.setNavigationItemSelectedListener { menuItem ->
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
             // Handle menu item selected
             menuItem.isChecked = true
-            activityMainBinding.drawerLayoutMenu.close()
+            binding.drawerLayoutMenu.close()
             true
         }
 
-        activityMainBinding.toolbar.setNavigationOnClickListener {
-            activityMainBinding.drawerLayoutMenu.open()
+        binding.toolbar.setNavigationOnClickListener {
+            binding.drawerLayoutMenu.open()
         }
 
+        updateUi(FirebaseAuth.getInstance().currentUser)
+
+    }
+
+    override fun onAuthStateChanged(firebaseAuth: FirebaseAuth) {
+        updateUi(firebaseAuth.currentUser)
+    }
+
+    private fun updateUi(currentUser : FirebaseUser?) {
+        if (currentUser == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        } else {
+            val navHeaderBinding =
+                HeaderNavigationDrawerBinding.bind(binding.navigationView.getHeaderView(0))
+            navHeaderBinding.loggedUserHeader.text = currentUser.email
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            1-> Firebase.auth.signOut()
+        }
+        //close navigation drawer
+        //close navigation drawer
+        binding.drawerLayoutMenu.close()
+        return true
     }
 }
